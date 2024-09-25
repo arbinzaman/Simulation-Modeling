242679f0d71aec2b0a8cfbb09eaaac5e66b31977
arbinzaman <arbinzmn@gmail.com>
2024-09-25 22:28:48 +0600

    Chore: Done second task

---
+++ Class 2/secondTask.m
@@ -0,0 +1,11 @@
+%Matlab condionals
+
+number = 8 ;
+if number > 10
+    fprintf("Number is greater than 10\n");
+    fprintf("Hello, World");
+elseif number ==  8
+    fprintf("Number is 8\n")
+else
+    fprintf("")
+end
