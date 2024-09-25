c8eff76b162849ceb24331b6c568165cda761ed0
arbinzaman <arbinzmn@gmail.com>
2024-09-25 22:26:42 +0600

    first commit

---
+++ Class 1/Class1.m
@@ -0,0 +1,34 @@
+y=[1 2]
+#disp (y)
+
+x = 1:10
+#disp(x)
+
+z= 1:2:10
+#disp(z)
+
+p = 10:-2: 1
+#disp(p)
+
+% q= 1:10
+% plot(x)
+% #disp(q)
+
+% r= 1:10
+% s=r
+% plot (r,s)
+% #disp(r)
+
+% a = 1: 10
+% b = [1 1 1 1 1 1 1 1 1 1]
+% plot(a,b)
+% #disp(x)
+
+c = 0 : 10
+d = sqrt(c)
+plot(c,d,'r')
+#disp(y(5))
+d(6) =100
+plot(c,d,'b')
+d(1:5)=[123 234 456 567 678]
+plot(c,d,'b')
